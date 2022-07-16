c530df3440a8d3080073a940ff1d65f423840f35
Your Name <you@example.com>
2022-07-16 13:15:13 +0500

    smart contract change again and inshall i will do more

--- contracts/Number.sol
+++ contracts/Number.sol
@@ -3,11 +3,15 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Num{
- 
- uint  public Get_number;
 
- function SetNumber ( uint _Taken_number) public {
-     Get_number = _Taken_number;
+  uint public getnumber;
+ function SetNumber ( uint _Taken_number) public{
+    getnumber =  _Taken_number;       
  }  
 
+function getNumber() public view returns(uint){
+    return getnumber;
+
+    
+}
 }
