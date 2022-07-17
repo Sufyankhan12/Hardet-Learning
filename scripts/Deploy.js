ebd1744b67313e59618eb970264c333cca1c7865
Your Name <you@example.com>
2022-07-17 11:33:32 +0500

    add commit where i can see and deploy easily main point

--- scripts/Deploy.js
+++ scripts/Deploy.js
@@ -15,3 +15,15 @@ main()
     console.error(error);
     process.exit(1);
   });
+
+// some importand points
+// 1.npx hardhat ndoe 
+// 2.npx hardhat compile
+// 3.npx hardhat run --network localhost scripts/Deploy.js
+// 4. npx hardhat console
+// 5. const Num = await ethers.getContractFactory("Num");
+// 6.   const num = await Num.deploy();
+          //  or const num = await Num.attach("contract addreess");
+// 7. await num.Setnumber(23);
+// just set and get number if some problem go to hardhat website
+ 
