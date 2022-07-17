38bc738587f14d6d2965867ba90de1646b617994
Your Name <you@example.com>
2022-07-17 11:33:07 +0500

    give and taken number error

--- contracts/Number.sol
+++ contracts/Number.sol
@@ -3,15 +3,11 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Num{
-
   uint public getnumber;
- function SetNumber ( uint _Taken_number) public{
-    getnumber =  _Taken_number;       
- }  
-
-function getNumber() public view returns(uint){
-    return getnumber;
-
-    
-}
+    function SetNumber ( uint _Taken_number) public{
+       getnumber =  _Taken_number;       
+     }  
+    function getNumber() public view returns(uint){
+       return getnumber;
+     }
 }
